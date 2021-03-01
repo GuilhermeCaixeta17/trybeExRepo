6d1711052d05e0b5cbcca7f2ab59d9a52f14849f
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-01 19:45:13 -0300

    adiciona Ex 1 de Funções a 4.4

--- Js/4.4/function+forIn.js
+++ Js/4.4/function+forIn.js
@@ -1,14 +1,15 @@
+//Objetos e For/In
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 //Ex 1 Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-  console.log("boas-vindas " + info['personagem'] + "!");
-  console.log("boas-vindas " + info.personagem + "!");
+console.log("boas-vindas " + info['personagem'] + "!");
+console.log("boas-vindas " + info.personagem + "!");
 //Ex 2 Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
-  info.recorrente = "Sim";
-  console.log(info);
+info.recorrente = "Sim";
+console.log(info);
 //Ex 3 Faça um for/in que mostre todas as chaves do objeto.
 for (const key in info) {
   console.log(key);
@@ -18,9 +19,23 @@ for (const key in info) {
   console.log(info[key]);
 }
 //Ex 5 Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
-let info2 ={
+let info2 = {
   personagem: "Tio Patinhas",
   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
   nota: "O último MacPatinhas",
   recorrente: "Sim",
 }
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Funções
+function palindromo(word) {
+  let result = false;
+  let isPalin = ""; 
+  for (let index = word.length -1; index  >= 0; index -= 1) {
+    isPalin += word[index];
+  }
+  if (word === isPalin) {
+    result = true;
+  }
+  return result;
+}
+console.log(palindromo("arara"));
