ac7c57be79d66b43ef4e80e1a7f3bc72a5cc6519
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-01 18:02:18 -0300

    adiciona Ex 2 a 4.4

--- Js/4.4/function+forIn.js
+++ Js/4.4/function+forIn.js
@@ -6,3 +6,6 @@ let info = {
 //Ex 1 Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
   console.log("boas-vindas " + info['personagem'] + "!");
   console.log("boas-vindas " + info.personagem + "!");
+//Ex 2 Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+  info.recorrente = "Sim";
+  console.log(info);
