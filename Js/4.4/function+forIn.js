2e5d340723a5db0c56bae35defd37342332c0fa6
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-01 18:34:04 -0300

    adiciona Ex 5 a 4.4

--- Js/4.4/function+forIn.js
+++ Js/4.4/function+forIn.js
@@ -13,7 +13,14 @@ let info = {
 for (const key in info) {
   console.log(key);
 }
-//Ex 4 aça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+//Ex 4 Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
 for (const key in info) {
   console.log(info[key]);
+}
+//Ex 5 Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+let info2 ={
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
 }
